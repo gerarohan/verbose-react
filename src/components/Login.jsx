d456a8435f9984262bec1ca5e44c4204002fef62
gerarohan <gerajonahrohan@gmail.com>
2022-05-23 13:57:01 +0100

    secound commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Login() {
+    return (
+        <>
+      
+      
+      hi
+        </>
+    )
+}
+
+export default Login;
